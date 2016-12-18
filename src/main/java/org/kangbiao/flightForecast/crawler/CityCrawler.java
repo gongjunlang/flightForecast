826565332ea4109ac8fd0142220a7a2115ab9e07
gongjunlang <1120147268@qq.com>
2016-12-18 15:10:57 +0800

    flightPrice

--- src/main/java/org/kangbiao/flightForecast/crawler/CityCrawler.java
+++ src/main/java/org/kangbiao/flightForecast/crawler/CityCrawler.java
@@ -1,98 +1,97 @@
 package org.kangbiao.flightForecast.crawler;
 
-import com.google.gson.Gson;
-import com.google.gson.reflect.TypeToken;
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.junit.Test;
-
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
+
+import org.jsoup.Jsoup;
+import org.junit.Test;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 
 /**
- * Created by kangbiao on 2016/12/17.
- * 爬取城市code信息
+ * Created by kangbiao on 2016/12/17. 爬取城市code信息
  */
 public class CityCrawler {
 
-    @Test
-    public void getCityCode() throws IOException {
-        String body = Jsoup.connect("http://www.tuniu.com/flight/").get().body().toString();
-        String cityString=body.split("var CitiesList = ")[1].split(";")[0];
-        Gson gson=new Gson();
-        List<CityInfo> cityList=gson.fromJson(cityString,new TypeToken<ArrayList<CityInfo>>() {}.getType());
-        for (CityInfo cityInfo:cityList){
-            System.out.println("INSERT INTO `city` VALUES('"+cityInfo.getCityName()+
-                    "','"+cityInfo.getCityCode()+
-                    "','"+cityInfo.getCityAbbr()+
-                    "','"+cityInfo.getCityEnglishName()+
-                    "','"+cityInfo.getCityIataCode()+
-                    "','"+cityInfo.getAbroad()+"')");
-        }
+	@Test
+	public void getCityCode() throws IOException {
+		String body = Jsoup.connect("http://www.tuniu.com/flight/").get()
+				.body().toString();
+		String cityString = body.split("var CitiesList = ")[1].split(";")[0];
+		System.out.println(cityString);
+		Gson gson = new Gson();
+		List<CityInfo> cityList = gson.fromJson(cityString,
+				new TypeToken<ArrayList<CityInfo>>() {
+				}.getType());
+		for (CityInfo cityInfo : cityList) {
+			System.out.println("INSERT INTO `city` VALUES('"
+					+ cityInfo.getCityName() + "','" + cityInfo.getCityCode()
+					+ "','" + cityInfo.getCityAbbr() + "','"
+					+ cityInfo.getCityEnglishName() + "','"
+					+ cityInfo.getCityIataCode() + "','" + cityInfo.getAbroad()
+					+ "')");
+		}
 
-
-//        System.out.print(body);
-    }
+		// System.out.print(body);
+	}
 
 }
 
-class CityInfo{
-    private String cityCode;
-    private String cityName;
-    private String cityAbbr;
-    private String cityEnglishName;
-    private String cityIataCode;
-    private String  abroad;
+class CityInfo {
+	private String cityCode;
+	private String cityName;
+	private String cityAbbr;
+	private String cityEnglishName;
+	private String cityIataCode;
+	private String abroad;
 
+	public String getCityCode() {
+		return cityCode;
+	}
 
-    public String getCityCode() {
-        return cityCode;
-    }
+	public void setCityCode(String cityCode) {
+		this.cityCode = cityCode;
+	}
 
-    public void setCityCode(String cityCode) {
-        this.cityCode = cityCode;
-    }
+	public String getCityName() {
+		return cityName;
+	}
 
-    public String getCityName() {
-        return cityName;
-    }
+	public void setCityName(String cityName) {
+		this.cityName = cityName;
+	}
 
-    public void setCityName(String cityName) {
-        this.cityName = cityName;
-    }
+	public String getCityAbbr() {
+		return cityAbbr;
+	}
 
-    public String getCityAbbr() {
-        return cityAbbr;
-    }
+	public void setCityAbbr(String cityAbbr) {
+		this.cityAbbr = cityAbbr;
+	}
 
-    public void setCityAbbr(String cityAbbr) {
-        this.cityAbbr = cityAbbr;
-    }
+	public String getCityEnglishName() {
+		return cityEnglishName;
+	}
 
-    public String getCityEnglishName() {
-        return cityEnglishName;
-    }
+	public void setCityEnglishName(String cityEnglishName) {
+		this.cityEnglishName = cityEnglishName;
+	}
 
-    public void setCityEnglishName(String cityEnglishName) {
-        this.cityEnglishName = cityEnglishName;
-    }
+	public String getCityIataCode() {
+		return cityIataCode;
+	}
 
-    public String getCityIataCode() {
-        return cityIataCode;
-    }
+	public void setCityIataCode(String cityIataCode) {
+		this.cityIataCode = cityIataCode;
+	}
 
-    public void setCityIataCode(String cityIataCode) {
-        this.cityIataCode = cityIataCode;
-    }
+	public String getAbroad() {
+		return abroad;
+	}
 
-    public String getAbroad() {
-        return abroad;
-    }
-
-    public void setAbroad(String abroad) {
-        this.abroad = abroad;
-    }
+	public void setAbroad(String abroad) {
+		this.abroad = abroad;
+	}
 }
--- src/main/java/org/kangbiao/flightForecast/crawler/FlightPriceCrawler.java
+++ src/main/java/org/kangbiao/flightForecast/crawler/FlightPriceCrawler.java
@@ -1,7 +1,28 @@
 package org.kangbiao.flightForecast.crawler;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+
+import org.junit.Test;
+
 /**
  * Created by kangbiao on 2016/12/17.
  */
 public class FlightPriceCrawler {
+	@Test
+	public void getFligtPrice() throws IOException {
+		URL doc = new URL(
+				"https://flight-api.tuniu.com/query/queryCalendarPrices?callback=jQuery172042180330051982584_1482042271205&{%22orgCityCode%22:%222802%22,%22dstCityCode%22:%22200%22,%22departureDate%22:%222016-12-18%22,%22type%22:1}&_=1482042274818");
+		BufferedReader in = new BufferedReader(new InputStreamReader(doc
+				.openConnection().getInputStream()));
+		String jsonString = "";
+		String currentString;
+		while ((currentString = in.readLine()) != null) {
+			jsonString += currentString;
+		}
+		jsonString = jsonString.split("\\(")[1].split("\\)")[0];
+		System.out.println(jsonString);
+	}
 }
