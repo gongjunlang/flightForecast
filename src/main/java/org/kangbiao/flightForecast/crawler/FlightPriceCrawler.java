b21a2b8a21494d3fccde78852881099e74f7c274
gongjunlang <1120147268@qq.com>
2016-12-22 18:03:19 +0800

    echart修改

--- src/main/java/org/kangbiao/flightForecast/crawler/FlightPriceCrawler.java
+++ src/main/java/org/kangbiao/flightForecast/crawler/FlightPriceCrawler.java
@@ -5,13 +5,30 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
 
+import org.jsoup.Jsoup;
 import org.junit.Test;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.sun.media.jfxmedia.control.VideoDataBuffer;
 
 /**
  * Created by kangbiao on 2016/12/17.
  *
  */
+@RestController
+@RequestMapping(value = "/json")
 public class FlightPriceCrawler {
-
-
+	@ResponseBody
+	@RequestMapping("/price")
+	public String getPrice() throws IOException {
+		String response = Jsoup
+				.connect(
+						"https://flight-api.tuniu.com/query/queryCalendarPrices?callback=jQuery17207817263226723168_1479379501967&%7B%22orgCityCode%22:%22906%22,%22dstCityCode%22:%222802%22,%22departureDate%22:%222016-12-09%22,%22type%22:1,%22backDate%22:%22%22%7D&_=1479379503757")
+				.ignoreContentType(true).execute().body();
+		String responseJson = response.split("\\(")[1].replace(")", "");
+		//System.out.println(11111);
+		return responseJson;
+	}
 }
--- src/main/webapp/js/index.js
+++ src/main/webapp/js/index.js
@@ -1,39 +1,71 @@
 /**
  * Created by kangbiao on 2016/12/17.
- *
+ * 
  */
 var myChart = echarts.init(document.getElementById('main'));
-var option = {
-    title: {
-        text: 'ECharts 入门示例'
-    },
-    tooltip: {},
-    legend: {
-        data:['销量']
-    },
-    xAxis: {
-        data: ["衬衫","羊毛衫","雪纺衫","裤子","高跟鞋","袜子"]
-    },
-    yAxis: {},
-    series: [{
-        name: '销量',
-        type: 'bar',
-        data: [5, 20, 36, 10, 10, 20]
-    }]
-};
-myChart.setOption(option);
+/*
+ * var option = { title : { text : '票价条形图' }, tooltip : {}, legend : { data : [
+ * '销量' ] }, xAxis : { data : [ "衬衫", "羊毛衫", "雪纺衫", "裤子", "高跟鞋", "袜子" ] }, yAxis :
+ * {}, series : [ { name : '销量', type : 'bar', data : [ 11, 20, 36, 10, 10, 20 ] } ] };
+ */
+// myChart.setOption(option);
 
-$("#confirm").click(function(){
-    $.ajax({
-        type: "get",
-        async: true,
-        url: "http://localhost:63342/fligh",
-        dataType: "json",
-        success: function (result) {
+$("#confirm")
+		.click(
+				function() {
+					$
+							.ajax({
+								type : "get",
+								async : true,
+								url : "http://localhost:8080/json/price",
+								dataType : "json",
+								success : function(result) {
+									// alert(result.data[0].date);
+									var dataString='';
+									var priceString='';
+									//alert(result.data)
+									$.each(result.data,function(i,p){
+										dataString+= '"'+p['date']+'"'+",";	
+										priceString+='"'+p['price']+'"'+",";
+									})
+									//alert(priceString);
+									var obj = JSON.parse("衬衫", "羊毛衫", "雪纺衫", "裤子", "高跟鞋", "袜子");
+									console.log(obj);
+									option = jsonChange('"衬衫", "羊毛衫", "雪纺衫", "裤子", "高跟鞋", "袜子"','"80","90","12","68","89","56"');
+									myChart.setOption(option);
+								},
+								error : function() {
+								}
+							});
+				});
 
-        },
-        error: function () {
 
-        }
-    });
-});
+function jsonChange(dataString,priceString){
+	var option = {
+			title : {
+				text : '票价条形图'
+			},
+			tooltip : {},
+			legend : {
+				data : [ '销量' ]
+			},
+			xAxis : {
+				//data : [ "衬衫", "羊毛衫", "雪纺衫", "裤子", "高跟鞋", "袜子" ]
+				data : [ dataString ]
+			},
+			yAxis : {},
+			series : [ {
+				name : '销量',
+				type : 'bar',
+				data : [ priceString ]
+			} ]
+		};
+	console.log(option);
+	return option;
+}
+/*
+ * $.each(data,function(i,p){ label[i]=p['label'];
+ * value[i]={'name':p['label'],'value':p['value']}; }); Chart.hideLoading();
+ * optionPie.legend.data=label; optionPie.series[0]['data']=value;
+ * optionPie.series[0]['radius']=[0,100]; Chart.setOption(optionPie);
+ */
